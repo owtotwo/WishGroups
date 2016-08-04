ddbe7972c45f60628ba03a63f3eb4cca47770271
owtotwo <owtotwo@163.com>
2016-08-04 13:28:36 +0800

    all

--- clean.sh
+++
@@ -1,8 +0,0 @@
-echo Cleaning...
-touch app/hey.pyc
-touch hey.pyc
-rm *.pyc
-find ./app/ -name *.pyc | xargs rm
-find ./ -name __pycache__ | xargs rm -r
-echo done!
-clear 
--- save.sh
+++
@@ -1,5 +0,0 @@
-./clean.sh
-echo Saving...
-git add --all
-git commit -a -m 'auto save'
-git push -u origin master
